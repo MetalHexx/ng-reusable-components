c165b9704cf8cc33d23b1c9e773864b597c1327e
William Pereira <MetalHexx@outlook.com>
2023-08-19 16:28:54 -0400

    Added new auto complete chips story

---
+++ src/app/autocomplete-chips/autocomplete-chips.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { AutocompleteChipsComponent } from './autocomplete-chips.component';
+
+const meta: Meta<AutocompleteChipsComponent> = {
+  title: 'App/AutoComplete-Chips',
+  component: AutocompleteChipsComponent,
+};
+
+export default meta;
+type Story = StoryObj<AutocompleteChipsComponent>;
+
+export const Primary: Story = {
+  render: () => ({
+    props: {
+      label: 'AutoComplete w/Chips',
+      primary: true,
+    },
+  }),
+};
