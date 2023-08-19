44e0292e1f0e3a0b4fb659070e45cc999c8f5fa0
William Pereira <MetalHexx@outlook.com>
2023-08-19 15:52:31 -0400

    added autocomplete-chips component

---
+++ src/app/autocomplete-chips/autocomplete-chips.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autocomplete-chips',
+  templateUrl: './autocomplete-chips.component.html',
+  styleUrls: ['./autocomplete-chips.component.scss']
+})
+export class AutocompleteChipsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
