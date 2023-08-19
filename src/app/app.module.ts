233d40ec5a13219008839d676139f0ab4caa723f
William Pereira <MetalHexx@outlook.com>
2023-08-19 16:34:13 -0400

    Added Angular Material to the angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AutocompleteChipsComponent } from './autocomplete-chips/autocomplete-chips.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AutocompleteChipsComponent } from './autocomplete-chips/autocomplete-ch
     AutocompleteChipsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
