7d47ace14384f699bb9a7009ccba59a4dad3eec0
William Pereira <MetalHexx@outlook.com>
2023-08-21 22:11:23 -0400

    Story book page, work in progress.

--- src/app/autocomplete-chips/autocomplete-chips.stories.ts
+++ src/app/autocomplete-chips/autocomplete-chips.stories.ts
@@ -1,19 +1,48 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { Meta, Story, applicationConfig, moduleMetadata } from '@storybook/angular';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { AutocompleteChipsComponent } from './autocomplete-chips.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialSharedModule } from '../material-shared/material-shared.module';
 
-const meta: Meta<AutocompleteChipsComponent> = {
+const sampleItems = [
+  { id: '1', display: 'Test1', value: 'Value1' },
+  { id: '2', display: 'Test2', value: 'Value2' },
+  { id: '3', display: 'Test3', value: 'Value3' }
+];
+
+export default {
   title: 'App/AutoComplete-Chips',
   component: AutocompleteChipsComponent,
-};
+  decorators: [
+    applicationConfig({
+      providers: [...provideAnimations()],
 
-export default meta;
-type Story = StoryObj<AutocompleteChipsComponent>;
+    }),
+    moduleMetadata({
+      imports: [MaterialSharedModule, ReactiveFormsModule]
+  })
+  ],
+  argTypes: {
+    items: {
+      name: 'items',
+      description: 'Array of items',
+      control: {
+        type: 'array'
+      }
+    }
+  }
+} as Meta;
 
-export const Primary: Story = {
-  render: () => ({
-    props: {
-      label: 'AutoComplete-Chips',
-      primary: true,
-    },
-  }),
+export const Primary: Story<AutocompleteChipsComponent<string>> = (args: AutocompleteChipsComponent<string>) => ({
+  props: {
+    //TODO: Getting strange console errors about ToLower() not being available.  Something strange happens when I pass the args here.
+    //...args,
+    placeholder: 'This is a place holder',
+    items: sampleItems
+  },
+});
+
+Primary.args = {
+  placeholder: 'This is a place holder',
+  //items: sampleItems
 };
